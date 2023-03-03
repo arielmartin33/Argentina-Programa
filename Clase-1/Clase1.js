82ecf0a0503daa3d6b3373112e5754e672b45b77
arielmartin33 <arielmartin33@gmail.com>
2023-03-02 23:13:20 -0300

    primeras clases

---
+++ Clase-1/Clase1.js
@@ -0,0 +1,17 @@
+let auto = 'Corolla';
+console.log(auto);
+
+auto = '308';
+console.log(auto);
+
+auto = 'C4';
+console.log(auto);
+
+let nombre = 'Ariel';
+let apellido = 'Martin';
+console.log(nombre + ' ' + apellido);
+
+let fruta = 'naranja';
+
+let tipoDeLaVariableFruta = typeof(fruta);
+console.log(tipoDeLaVariableFruta);
